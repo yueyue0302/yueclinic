b9e099923749faf88440e2e6567cf276adc5619b
yueyue0302 <yueyue0302@users.noreply.github.com>
2026-06-02 22:35:37 +0900

    update: Google口コミリンクを設定

--- src/lib/clinicMap.ts
+++ src/lib/clinicMap.ts
@@ -1,8 +1,10 @@
 const googleMapsCid = '10842190342173572710';
+const googleMapsFeatureId = '0x0:0x967732abfc6d3e66';
 
 export const clinicGoogleMapsUrl = `https://www.google.com/maps?cid=${googleMapsCid}`;
 
-export const clinicGoogleReviewUrl = clinicGoogleMapsUrl;
+export const clinicGoogleReviewUrl =
+  `https://www.google.com/search?q=yueclinic&ludocid=${googleMapsCid}#lrd=${googleMapsFeatureId},3,,,`;
 
 export const clinicGoogleMapsEmbedUrl =
   `https://www.google.com/maps/embed?origin=mfe&pb=!1m4!3m2!1m1!4s${googleMapsCid}!6i17`;
